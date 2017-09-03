907225109fa148093b5b518cdf7e3fdeebd53183
yaxinsn <yaxinsn@163.com>
2017-09-03 13:54:43 +0800

    loadfile，lua是解释性语言，文本会被预编译，这样会提高执行效率。

--- 8.0.load_strings.lua
+++ 8.0.load_strings.lua
@@ -1,6 +1,11 @@
 
-
-
+--lua解释性语言，在解释过程中，也是很文本预编译成中间码，再去执行中间码。主要是为了提高效率，
+--被编译成中间码的子函数，不需要被编译多次了。中间码执行效率也会提高。
+--解释器 ==编译器。
+--loadfile,dofile.
+--loadstring
+--loadfile 只编译一次。而dofile每次都编译。
+--
 
 
 print("please input you expression");
