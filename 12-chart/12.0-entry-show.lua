140f3907337ff7d13f5565fc95552c9e09d6681e
yaxinsn <yaxinsn@163.com>
2017-09-13 13:36:40 +0800

    12-chart test and example

---
+++ 12-chart/12.0-entry-show.lua
@@ -0,0 +1,6 @@
+
+
+dofile "12.0-entry.lua"
+
+
+print("The count of entry is " .. count);
---
+++ 12-chart/12.0-entry.lua
@@ -0,0 +1,15 @@
+
+count = 0
+function Entry (b) count = count + 1 end
+
+Entry{"Donald E. Knuth",
+"Literate Programming",
+"CSLI",
+1992}
+Entry{"Jon Bentley",
+"More Programming Pearls",
+"Addison-Wesley",
+1990}
+
+
+
