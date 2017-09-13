97ab8e48329806f8ccecac359b1147560d652a38
yaxinsn <yaxinsn@163.com>
2017-09-13 15:05:16 +0800

    modify 12.0-chart

--- 12-chart/12.0-entry-show.lua
+++ 12-chart/12.0-entry-show.lua
@@ -1,5 +1,7 @@
 
 
+count = 0
+function Entry (b) count = count + 1 end
 dofile "12.0-entry.lua"
 
 
--- 12-chart/12.0-entry.lua
+++ 12-chart/12.0-entry.lua
@@ -1,15 +1,13 @@
 
-count = 0
-function Entry (b) count = count + 1 end
 
 Entry{"Donald E. Knuth",
-"Literate Programming",
-"CSLI",
-1992}
+	"Literate Programming",
+	"CSLI",
+	1992}
 Entry{"Jon Bentley",
-"More Programming Pearls",
-"Addison-Wesley",
-1990}
+	"More Programming Pearls",
+	"Addison-Wesley",
+	1990}
 
 
 
