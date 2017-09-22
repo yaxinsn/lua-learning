2a5731798149d98016930d00ba6793d904204351
yaxinsn <yaxinsn@163.com>
2017-09-22 11:06:30 +0800

    add comment at 13.4-Window.lua

--- 12-13-14-chart/13.4-Window.lua
+++ 12-13-14-chart/13.4-Window.lua
@@ -1,4 +1,8 @@
-
+--学习__index.和__newindex
+--__index是以后类继承用的。
+--前面说过，当我们访问一个表的不存在的域，返回结果为 nil，这是正确的，但并不
+--一致正确。实际上，这种访问触发 lua 解释器去查找__index metamethod：如果不存在，
+--返回结果为 nil；如果存在则由__index metamethod 返回结果
 
 Window = {}
 -- create the prototype with default values
@@ -9,11 +13,12 @@ function Window.new (o)
 	setmetatable(o, Window.mt) --这个函数有魔力 。
 	return o
 end
+--__index方式之一：可以是配置成一个函数。
 Window.mt.__index = function (table, key) --定义了一个__index，这是个关键字，表示一个运算符所对应的处理函数。
 	--return Window.prototype[key]
 	return "no this key <" .. key .. "> bye"
 end
-
+--__index方式之二： 也可是一个表。
 Window.mt.__index = Window.prototype;
 
 
